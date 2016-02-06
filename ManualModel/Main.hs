0924af591e8d739828cee0e1dd7d78b28d0f3248
M. G. Meier <mgoremeier@gmail.com>
2016-02-06 18:03:11 +0100

    Scene 3 WIP

--- ManualModel/Main.hs
+++ ManualModel/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 
 import IconGuesser
 
@@ -19,19 +19,21 @@ import Debug.Trace
 data ConeDemo = ConeDemo
     { basePrefix        :: String
     , baseColorsWeb     :: Maybe [String]
+    , baseModifiers     :: Maybe [[Float]]
     , theTree           :: ConeTree
     }
 
 
 instance ToJSON ConeDemo where
-    toJSON (ConeDemo basePref cols tree) =
+    toJSON ConeDemo {..} =
         let
-            ~(Object o1) = cleanup $ toJSON tree
+            ~(Object o1) = cleanup $ toJSON theTree
             ~(Object o2) = object
-                [ "basePrefix" .= basePref
-                , "baseColors" .= cols
+                [ "basePrefix"          .= basePrefix
+                , "baseColors"          .= baseColorsWeb
+                , "baseColorModifiers"  .= baseModifiers
                 ]
-        in Object $ o1 `HM.union` o2
+        in Object $ o2 `HM.union` o1
 
 
 -- HELPERS
@@ -51,7 +53,8 @@ node :: ConeEntry -> [ConeTree] -> ConeTree
 node e [] = RoseLeaf e {ceIsLeaf = ceIsLeaf e && True} (-1) []
 node e cs = RoseLeaf e {ceIsLeaf = False} (-1) cs
 
-
+coneNodes :: [ConeTree] -> [ConeTree]
+coneNodes = map (\(RoseLeaf a b cs) -> RoseLeaf a {ceIsLeaf = False} b cs)
 
 -- MAIN
 
@@ -66,25 +69,56 @@ main = do
     let
         coneDemo = ConeDemo
             "base"
-            (Just ["#4532A2", "#222222"])
+            (Just ["#273f61", "#325765", "#4d818c"])
+            (Just [[0.95, 0.95, 0.95], [0.85, 0.85, 0.85], [0.8, 0.8, 0.8]])
             (applyIconGuesser iconGuesser root)
 
-    B.writeFile "exportModel.json" $ encodePretty $  coneDemo
+    B.writeFile "exportModel.json" $ encodePretty $ coneDemo
 
 
 
 -- MANUAL DEFINITION
 
 root :: ConeTree
-root = node (entry "Business Needs") [financial]
+root = node (entry "Business Needs") [legal, financial, infrastructure]
 
-financial = node (entry "Financial") [management]
 
-management = node (entry "Management") [productivity, facilities]
+legal = node (entry "Legal") $ leaves True $ map entry
+    ["Health & Safety", "Accouting", "Human Resources", "Insurance"]
 
-facilities = node (entry "Facilities") []
 
+financial = node (entry "Financial") [prod, sales, management]
+
+management = node (entry "Management") $ coneNodes
+    [ productivity
+    , facilities
+    , location
+    , licensing
+    , software
+    , humRes
+    ]
+facilities  = node (entry "Facilities") []
+location    = node (entry "Location") []
+licensing   = node (entry "Licensing") []
+software    = node (entry "Software") []
+humRes      = node (entry "Human Resources") hrChildren
+  where
+    hrChildren = leaves True . map entry $
+        ["Training", "Recruitment", "Communication"]
 productivity = node (entry "Productivity") prodChildren
   where
     prodChildren = leaves True . map entry $
         ["Dashboards", "Analytics", "Reporting", "Trends", "BPM", "Operations", "Logistics"]
+
+
+sales = node (entry "Sales") $ leaves True $ map entry
+    ["Distribution", "Processing", "Customer Service", "Selling", "Marketing", "Market Research"]
+
+prod = node (entry "Product") $ leaves True $ map entry
+    ["Skills", "Staff", "Raw Materials", "Suppliers", "Innovation", "Potential", "Competitiveness"]
+
+
+
+
+
+infrastructure = node (entry "Infrastructure") []
